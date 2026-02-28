ec4e311bf4c2e5218307bddf4fe6f6e37768bbd0
Sandip 1401 <sandipsongra14@gmail.com>
2026-02-28 14:03:07 +0530

    initial commit

---
+++ src/config/datasource.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import dotenv from "dotenv";
+dotenv.config();
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  schema: "public",
+  synchronize: false,
+  logging: true,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+});
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import "dotenv/config";
+import app from "./app";
+import { AppDataSource } from "./config/datasource";
+
+const PORT = Number(process.env.PORT) || 8000;
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed", error);
+  });
