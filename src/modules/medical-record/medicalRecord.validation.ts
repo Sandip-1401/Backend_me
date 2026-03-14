93a2207c136fa7289924ff8bf063a664e5ddefd5
Sandip 1401 <sandipsongra14@gmail.com>
2026-03-14 20:15:52 +0900

    comment doctorid and patientid in medical ecord validation zod

--- src/modules/medical-record/medicalRecord.validation.ts
+++ src/modules/medical-record/medicalRecord.validation.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const createMedicalRecordSchema = z.object({
-  patient_id: z.string().uuid(),
-  doctor_id: z.string().uuid(),
+  // patient_id: z.string().uuid(),
+  // doctor_id: z.string().uuid(),
   appointment_id: z.string().uuid(),
   diagnosis: z.string().min(3),
   notes: z.string().optional(),
